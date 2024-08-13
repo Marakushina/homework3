0cbbf77ab0376e6d81fb5a970b8c5b97bb50639f
Marakushina <marakushina970@mail.ru>
2024-08-13 23:43:51 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,5 @@
+Number_of_comleted_homework=12
+Number_of_hour_sent=1.5
+Name_of_curse="Python"
+Time_for_one_task=(Number_of_hour_sent/Number_of_comleted_homework)
+print("курс:",Name_of_curse,",","всего задач:",Number_of_comleted_homework,",","затрачено часов:",Number_of_hour_sent,",","среднее время заполнения:",Time_for_one_task)
